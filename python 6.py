bd2d20146d9062cd08c60e99d6d3a4fd3339248e
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-01-21 23:54:58 +0530

    Add files via upload

---
+++ python 6.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+12.Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the 
+number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+
+# In[1]:
+
+
+for i in range(1000,3001):
+    i=str(i)
+    c=0
+    for j in i:
+        j=int(j)
+        if(j%2==0):
+            c=c+1
+        else:
+            pass
+    if c==4:
+        print(i,end=',')  
+    else:
+        pass
+
+
+# In[ ]:
+
+
+13. Write a program that accepts a sentence and calculate the number of letters and digits. 
+Suppose the input is supplied to the program: hello world! 123 
+Then, the output should be: LETTERS 10
+DIGITS 3 
+
+
+# In[2]:
+
+
+n=input('enter letters and digits')
+c=0
+d=0
+for i in n:
+    if i.isalpha():
+        c=c+1
+    elif i.isdigit():
+        d=d+1
+    else:
+        pass
+print('Letters:',c)
+print('Digits:',d)
+
+
+# In[ ]:
+
+
+
+
