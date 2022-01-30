83de047c964ba1db611598712ee9a36dce978deb
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-01-30 23:11:07 +0530

    Add files via upload

---
+++ python 13.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+26.Define a function which can compute the sum of two numbers. 
+Hints: Define a function with two numbers as arguments. You can compute the sum in the function and return the value. 
+
+
+# In[1]:
+
+
+def add(a,b):
+    
+    c=a+b
+    
+    return (c)
+
+add(10,50)
+
+
+# In[ ]:
+
+
+27.Define a function that can convert a integer into a string and print it in console. 
+Hints: Use str() to convert a number to string. 
+
+
+# In[2]:
+
+
+def integer():
+    
+    a=int(input('enter'))
+    
+    b=str(a)
+    
+    return(b)
+
+integer()
+
+
+# In[ ]:
+
+
+
+
