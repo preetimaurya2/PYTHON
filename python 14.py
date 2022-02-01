2c8c10a06441adb9b4972560efdb86acd17e47e5
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-02-01 23:13:58 +0530

    Add files via upload

---
+++ python 14.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+28.Define a function that can receive two integral numbers in string form and compute their sum and then print it in 
+console. 
+
+
+# In[ ]:
+
+
+def add(a,b):
+    
+    return(int(a)+int(b))
+
+add('12','34')
+
+
+# In[ ]:
+
+
+29. Define a function that can accept two strings as input and concatenate them and then print it in console. 
+Hints: Use + to concatenate the strings 
+
+
+# In[ ]:
+
+
+def string():
+    
+    n1=input('enter string1:')
+    n2=input('enter string2:')
+    
+    print(n1+n2)
+    
+string()    
+
