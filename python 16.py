3f62ae043594ddf32706a0e020318aa2a60d4495
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-02-04 23:08:07 +0530

    Add files via upload

---
+++ python 16.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+33.Define a function which can print a dictionary where the keys are numbers between 1 and 3 (both included) and the values
+are square of key
+
+
+# In[ ]:
+
+
+def dic():
+    
+    c={i:i**2 for i in range(1,4)}
+    
+    print(c)
+    
+dic()
+
+
+# In[ ]:
+
+
+34.Define a function which can print a dictionary where the keys are numbers between 1 and 20 (both included) and the 
+values are square of keys.
+
+
+# In[ ]:
+
+
+def dic():
+    
+    c={i:i**2 for i in range(1,21)}
+    
+    print(c)
+    
+dic() 
+
