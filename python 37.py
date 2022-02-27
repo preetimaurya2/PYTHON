b5715fcd2c860cacd63baf6f2abd3164e9332508
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-02-27 20:46:23 +0530

    Add files via upload

---
+++ python 37.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+67. Please write a program which accepts basic mathematic expression from console and print the evaluation result. 
+If the following string is given as input to the program: 35+3 
+Then, the output of the program should be: 38 
+Hints: Use eval() to evaluate an expression. 
+
+
+# In[1]:
+
+
+a=eval(input('please enter:'))
+print(a)
+
