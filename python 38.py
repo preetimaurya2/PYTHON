cf938331fd336af82e6cc1d913fbe8ad3e488563
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-02-28 21:20:06 +0530

    Add files via upload

---
+++ python 38.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+68.Please write assert statements to verify that every number in the list [2,4,6,8] is even. 
+Hints: Use "assert expression" to make assertion. 
+
+
+# In[4]:
+
+
+a=[2,4,6,8]
+for i in a:
+    assert i%2==0
+
