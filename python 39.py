b2638136cf8461fa12518cedeee85fed7d0f3c74
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-03-02 22:35:26 +0530

    Add files via upload

---
+++ python 39.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+69. Please generate a random float where the value is between 10 and 100 using Python math module. 
+Hints: Use random.random() to generate a random float in [0,1]. 
+
+
+# In[ ]:
+
+
+import random
+a=[i for i in range(10,100)]
+print(random.random())
+
