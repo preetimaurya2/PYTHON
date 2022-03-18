19fa7dc996f6d6266bda0ae3c54c89642e68f0c9
preetimaurya2 <97082407+preetimaurya2@users.noreply.github.com>
2022-03-18 23:55:37 +0530

    Add files via upload

--- python 47.py
+++ python 47.py
@@ -4,30 +4,32 @@
 # In[ ]:
 
 
-83. By using list comprehension, please write a program generate a 3*5*8 3D array whose each element is 0. 
-Hints: Use list comprehension to make an array. 
+85. By using list comprehension, please write a program to print the list after removing the value 24 in
+[12,24,35,24,88,120,155]. 
+Hints: Use list's remove method to delete a value. 
 
 
-# In[3]:
+# In[ ]:
 
 
-a=[[[0*(i*j*k) for j in range(3)]for i in range(5)]for k in range(8)]
+a=[12,24,35,24,88,120,155]
+a.remove(24)
 print(a)
 
 
 # In[ ]:
 
 
-84. By using list comprehension, please write a program to print the list after removing the 0th,4th,5th numbers in 
-[12,24,35,70,88,120,155]. 
-Hints: Use list comprehension to delete a bunch of element from a list. Use enumerate() to get (index, value) tuple. 
+86. With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements are 
+intersection of the above given lists. 
+Hints: Use set() and "&=" to do set intersection operation. 
 
 
-# In[5]:
+# In[4]:
 
 
-a=[12,24,35,70,88,120,155]
-b=[0,4,5]
-c=[x for (i,x) in enumerate(a) if i not in b]
+a=[1,3,6,78,35,55]
+b=[12,24,35,24,88,120,155]
+c=set(a).intersection(b)
 print(c)
 
